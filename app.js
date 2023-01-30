05b1a8f16c0496f272a95fa5f1e531a15c0c4dfa
scottCheung <scottcx2@gmail.com>
2023-01-30 01:25:17 -0500

    tested for production release

--- app.js
+++ app.js
@@ -26,56 +26,59 @@ const projects = [
 ]
 
 const displayProjects = () => {
-    console.log('display projects');
-    
+   
     projects.forEach((project) => {
         const latest = document.querySelector('.recent');
 
-        const projectContainer = document.createElement('div');
-        projectContainer.classList.add('project');
-        latest.appendChild(projectContainer);
+        if (latest !== null) {
+            const projectContainer = document.createElement('div');
+            projectContainer.classList.add('project');
+            latest.appendChild(projectContainer);
+    
+            const imageContainer = document.createElement('div');
+            imageContainer.classList.add('imgContainer');
+            projectContainer.appendChild(imageContainer);
+    
+            const projectImage = document.createElement('img');
+            projectImage.setAttribute('src', project.preview);
+            projectImage.setAttribute('alt','image of webpage');
+            projectImage.classList.add('projectImg')
+            imageContainer.appendChild(projectImage);
+    
+            const projectInfo = document.createElement('div');
+            projectInfo.classList.add('projText');
+            projectContainer.appendChild(projectInfo);
+    
+            const projectTitle = document.createElement('h3');
+            projectTitle.innerText = project.title;
+            projectInfo.appendChild(projectTitle);
+    
+            const projectStack = document.createElement('p');
+            projectStack.innerText = project.stackUsed;
+            projectStack.classList.add('stack');
+            projectInfo.appendChild(projectStack);
+            
+            const projectDescription = document.createElement('p');
+            projectDescription.innerText = project.description;
+            projectDescription.classList.add('description');
+            projectInfo.appendChild(projectDescription);
+    
+            const linkButtons = document.createElement('div');
+            linkButtons.classList.add('linkButton');
+            projectContainer.appendChild(linkButtons);
+    
+            const projectLive = document.createElement('a');
+            projectLive.setAttribute('href', project.live);
+            projectLive.innerText = 'View Live';
+            linkButtons.appendChild(projectLive);
+    
+            const projectCode = document.createElement('a');
+            projectCode.setAttribute('href', project.code)
+            projectCode.innerText = 'View Code';
+            linkButtons.appendChild(projectCode);
 
-        const imageContainer = document.createElement('div');
-        imageContainer.classList.add('imgContainer');
-        projectContainer.appendChild(imageContainer);
+        } else return;
 
-        const projectImage = document.createElement('img');
-        projectImage.setAttribute('src', project.preview);
-        projectImage.setAttribute('alt','image of webpage');
-        projectImage.classList.add('projectImg')
-        imageContainer.appendChild(projectImage);
-
-        const projectInfo = document.createElement('div');
-        projectInfo.classList.add('projText');
-        projectContainer.appendChild(projectInfo);
-
-        const projectTitle = document.createElement('h3');
-        projectTitle.innerText = project.title;
-        projectInfo.appendChild(projectTitle);
-
-        const projectStack = document.createElement('p');
-        projectStack.innerText = project.stackUsed;
-        projectStack.classList.add('stack');
-        projectInfo.appendChild(projectStack);
-        
-        const projectDescription = document.createElement('p');
-        projectDescription.innerText = project.description;
-        projectDescription.classList.add('description');
-        projectInfo.appendChild(projectDescription);
-
-        const linkButtons = document.createElement('div');
-        linkButtons.classList.add('linkButton');
-        projectContainer.appendChild(linkButtons);
-
-        const projectLive = document.createElement('a');
-        projectLive.setAttribute('href', project.live);
-        projectLive.innerText = 'View Live';
-        linkButtons.appendChild(projectLive);
-
-        const projectCode = document.createElement('a');
-        projectCode.setAttribute('href', project.code)
-        projectCode.innerText = 'View Code';
-        linkButtons.appendChild(projectCode);
 
     })
 }
